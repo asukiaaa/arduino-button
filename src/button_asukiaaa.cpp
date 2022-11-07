a92b2fda73cc16d5615cd4ebc4923e8c141a31fd
Asuki Kono <asukiaaa@gmail.com>
2022-11-07 09:46:19 +0900

    Update indent with using formatter

--- src/button_asukiaaa.cpp
+++ src/button_asukiaaa.cpp
@@ -1,63 +1,62 @@
 #include "button_asukiaaa.h"
 
 namespace button_asukiaaa {
-  ButtonState::ButtonState(bool pressedPinState, unsigned long bufferMs) {
-    this->bufferMs = bufferMs;
-    this->pressedPinState = pressedPinState;
-    changedAt = 0;
-    prevPinState = currentPinState = holedPinState = !pressedPinState;
-  }
+ButtonState::ButtonState(bool pressedPinState, unsigned long bufferMs)
+    : pressedPinState(pressedPinState) {
+  this->bufferMs = bufferMs;
+  changedAt = 0;
+  prevPinState = currentPinState = holedPinState = !pressedPinState;
+}
 
-  void ButtonState::update(bool newState) {
-    prevPinState = currentPinState;
-    currentPinState = newState;
-    if (currentPinState != prevPinState) {
-      if (currentPinState == holedPinState) {
-        // detect changing state as noize
-        changedOverBufferTime = true;
-      } else {
-        changedAt = millis();
-        changedOverBufferTime = false;
-        onOverBufferTime = false;
-      }
-    }
-    if (changedOverBufferTime && onOverBufferTime) {
-      onOverBufferTime = false; // become false after first time of over buffer time
-    }
-    if (!changedOverBufferTime && millis() - changedAt > bufferMs) {
+void ButtonState::update(bool newState) {
+  prevPinState = currentPinState;
+  currentPinState = newState;
+  if (currentPinState != prevPinState) {
+    if (currentPinState == holedPinState) {
+      // detect changing state as noize
       changedOverBufferTime = true;
-      onOverBufferTime = true;
-      holedPinState = currentPinState;
+    } else {
+      changedAt = millis();
+      changedOverBufferTime = false;
+      onOverBufferTime = false;
     }
   }
-
-  bool ButtonState::changedToPress() const {
-    return currentPinState == pressedPinState && onOverBufferTime;
+  if (changedOverBufferTime && onOverBufferTime) {
+    onOverBufferTime =
+        false;  // become false after first time of over buffer time
   }
-  bool ButtonState::changedToRelease() const {
-    return currentPinState != pressedPinState && onOverBufferTime;
-  }
-  bool ButtonState::isPresseing() const {
-    return holedPinState == pressedPinState;
-  }
-  bool ButtonState::isReleasing() const {
-    return holedPinState != pressedPinState;
-  }
-
-  Button::Button(int pin,
-                 unsigned long bufferMs,
-                 int pinModeOption,
-                 bool pressedPinState)
-    : ButtonState(pressedPinState, bufferMs) {
-    this->pin = pin;
-    this->pinModeOption = pinModeOption;
-  }
-
-  void Button::begin() {
-    pinMode(pin, pinModeOption);
-  }
-
-  void Button::update() {
-    ButtonState::update(digitalRead(pin));
+  if (!changedOverBufferTime && millis() - changedAt > bufferMs) {
+    changedOverBufferTime = true;
+    onOverBufferTime = true;
+    holedPinState = currentPinState;
   }
 }
+
+bool ButtonState::changedToPress() const {
+  return currentPinState == pressedPinState && onOverBufferTime;
+}
+bool ButtonState::changedToRelease() const {
+  return currentPinState != pressedPinState && onOverBufferTime;
+}
+bool ButtonState::isPresseing() const {
+  return holedPinState == pressedPinState;
+}
+bool ButtonState::isReleasing() const {
+  return holedPinState != pressedPinState;
+}
+
+bool ButtonState::rawCurrentPressed() const {
+  return currentPinState == pressedPinState;
+}
+
+Button::Button(int pin, unsigned long bufferMs, int pinModeOption,
+               bool pressedPinState)
+    : ButtonState(pressedPinState, bufferMs) {
+  this->pin = pin;
+  this->pinModeOption = pinModeOption;
+}
+
+void Button::begin() { pinMode(pin, pinModeOption); }
+
+void Button::update() { ButtonState::update(digitalRead(pin)); }
+}  // namespace button_asukiaaa
--- src/button_asukiaaa.h
+++ src/button_asukiaaa.h
@@ -4,42 +4,41 @@
 #include <Arduino.h>
 
 namespace button_asukiaaa {
-  class ButtonState {
-  public:
-    ButtonState(bool pressedPinState = HIGH, unsigned long bufferMs = 20UL);
+class ButtonState {
+ public:
+  ButtonState(bool pressedPinState = HIGH, unsigned long bufferMs = 20UL);
 
-    void update(bool newState);
-    bool changedToPress() const;
-    bool changedToRelease() const;
-    bool isPresseing() const;
-    bool isReleasing() const;
+  void update(bool newState);
+  bool changedToPress() const;
+  bool changedToRelease() const;
+  bool isPresseing() const;
+  bool isReleasing() const;
+  bool rawCurrentPressed() const;
 
-  private:
-    unsigned long bufferMs;
-    unsigned long changedAt;
-    char timingState;
-    bool onOverBufferTime;
-    bool changedOverBufferTime;
-    bool currentPinState;
-    bool changedPinState;
-    bool prevPinState;
-    bool holedPinState;
-    bool pressedPinState;
-  };
+ private:
+  unsigned long bufferMs;
+  unsigned long changedAt;
+  char timingState;
+  bool onOverBufferTime;
+  bool changedOverBufferTime;
+  bool currentPinState;
+  bool changedPinState;
+  bool prevPinState;
+  bool holedPinState;
+  bool pressedPinState;
+};
 
-  class Button: public ButtonState {
-  public:
-    Button(int pin,
-           unsigned long bufferMs = 20UL,
-           int pinModeOption = INPUT_PULLUP,
-           bool pressedPinState = LOW);
-    void begin();
-    void update();
+class Button : public ButtonState {
+ public:
+  Button(int pin, unsigned long bufferMs = 20UL,
+         int pinModeOption = INPUT_PULLUP, bool pressedPinState = LOW);
+  void begin();
+  void update();
 
-  private:
-    int pin;
-    int pinModeOption;
-  };
-}
+ private:
+  int pin;
+  int pinModeOption;
+};
+}  // namespace button_asukiaaa
 
 #endif
